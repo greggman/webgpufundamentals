02ac64ab16f7ce419cdfb18f0bd1bb6bfbee4f11
greggman <github@greggman.com>
2024-06-05 20:49:48 +0000

    Deploying to gh-pages from  @ a0b28c84722d12bc3e907ca218b8dcdb70a35d0e 🚀

---
+++ webgpu/lessons/resources/lesson.js
@@ -0,0 +1,109 @@
+import '/webgpu/lessons/resources/data-tables.js';
+// Licensed under a BSD license. See license.html for license
+/* eslint-disable strict */
+/* global settings, contributors, jQuery */
+(function($){
+
+function getQueryParams() {
+  return Object.fromEntries(new URLSearchParams(window.location.search).entries());
+}
+
+//
+function replaceParams(str, subs) {
+  return str.replace(/\${(\w+)}/g, function(m, key) {
+    return subs[key];
+  });
+}
+
+function showContributors() {
+  // contribTemplate: 'Thank you
+  // <a href="${html_url}">
+  // <img src="${avatar_url}">${login}<a/>
+  //  for <a href="https://github.com/${owner}/${repo}/commits?author=${login}">${contributions} contributions</a>',
+  try {
+    const subs = {...settings, ...contributors[Math.random() * contributors.length | 0]};
+    const template = settings.contribTemplate;
+    const html = replaceParams(template, subs);
+    const parent = document.querySelector('#forkongithub a');
+    const div = document.createElement('div');
+    div.className = 'contributors';
+    div.innerHTML = html;
+    parent.appendChild(div);
+  } catch (e) {
+    console.error(e);
+  }
+}
+showContributors();
+
+$(document).ready(function($) {
+  const linkImgs = function(bigHref) {
+    return function() {
+      const a = document.createElement('a');
+      a.href = bigHref;
+      a.title = this.alt;
+      a.className = this.className;
+      a.setAttribute('align', this.align);
+      this.setAttribute('align', '');
+      this.className = '';
+      this.style.border = '0px';
+      return a;
+    };
+  };
+  const linkSmallImgs = function(ext) {
+    return function() {
+      const src = this.src;
+      return linkImgs(src.substr(0, src.length - 7) + ext);
+    };
+  };
+  const linkBigImgs = function() {
+    const src = $(this).attr('big');
+    return linkImgs(src);
+  };
+  $('img[big$=".jpg"]').wrap(linkBigImgs);
+  $('img[src$="-sm.jpg"]').wrap(linkSmallImgs('.jpg'));
+  $('img[src$="-sm.gif"]').wrap(linkSmallImgs('.gif'));
+  $('img[src$="-sm.png"]').wrap(linkSmallImgs('.png'));
+  $('pre>code')
+     .unwrap()
+     .replaceWith(function() {
+      return $('<pre class="prettyprint showlinemods notranslate" translate="no">' + this.innerHTML + '</pre>');
+    });
+  if (window.prettyPrint) {
+    window.prettyPrint();
+    // Firefox doesn't support the has() css selector as of 2023-09-09
+    try {
+      document.querySelectorAll('pre:has(.linedeleted)').forEach(e => {
+        const b = $('<button>').text('hide deleted').addClass('linedeleted-button').attr('type', 'button').on('click', () => {
+          const hide = e.classList.toggle('hide-linedeleted');
+          b.text(hide ? 'show deleted' : 'hide deleted');
+        });
+        $(e).append(b);
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  $('span[class=com]')
+    .addClass('translate yestranslate')
+    .attr('translate', 'yes');
+
+  const params = getQueryParams();
+  if (params.doubleSpace || params.doublespace) {
+    document.body.className = document.body.className + ' doubleSpace';
+  }
+
+  $('.language').on('change', function() {
+    window.location.href = this.value;
+  });
+
+  $('a[data-href]').on('click', function() {
+    window.location.href = this.dataset.href;
+  });
+
+  $('[data-table]').html();
+
+});
+}(jQuery));
+
+// ios needs this to allow touch events in an iframe
+window.addEventListener('touchstart', {});
---
+++ webgpu/lessons/webgpu-limits-and-features.js
@@ -0,0 +1,110 @@
+import {
+  renderDiagrams
+} from './resources/diagrams.js';
+import {
+  makeTable,
+} from './resources/elem.js';
+import { shortSize } from './resources/utils.js';
+
+export const kMaxUnsignedLongValue = 4294967295;
+export const kMaxUnsignedLongLongValue = Number.MAX_SAFE_INTEGER;
+
+function valuesToObject(propertyNames, defaults, values) {
+  return Object.fromEntries(propertyNames.map((propName, ndx) => [propName, values[ndx] || defaults[ndx]]));
+}
+function makeObjectFromTable(propertyNames, defaults, values) {
+  return Object.fromEntries(Object.entries(values).map(([propName, values]) => [
+    propName, valuesToObject(propertyNames, defaults, values),
+  ]));
+}
+
+/* eslint-disable no-sparse-arrays */
+const kLimitInfo = makeObjectFromTable(
+                                               [    'class', 'default',            'maximumValue'],
+                                               [  'maximum',          ,     kMaxUnsignedLongValue], {
+  'maxTextureDimension1D':                     [           ,      8192,                          ],
+  'maxTextureDimension2D':                     [           ,      8192,                          ],
+  'maxTextureDimension3D':                     [           ,      2048,                          ],
+  'maxTextureArrayLayers':                     [           ,       256,                          ],
+
+  'maxBindGroups':                             [           ,         4,                          ],
+  'maxBindingsPerBindGroup':                   [           ,      1000,                          ],
+  'maxDynamicUniformBuffersPerPipelineLayout': [           ,         8,                          ],
+  'maxDynamicStorageBuffersPerPipelineLayout': [           ,         4,                          ],
+  'maxSampledTexturesPerShaderStage':          [           ,        16,                          ],
+  'maxSamplersPerShaderStage':                 [           ,        16,                          ],
+  'maxStorageBuffersPerShaderStage':           [           ,         8,                          ],
+  'maxStorageTexturesPerShaderStage':          [           ,         4,                          ],
+  'maxUniformBuffersPerShaderStage':           [           ,        12,                          ],
+
+  'maxUniformBufferBindingSize':               [           ,     65536, kMaxUnsignedLongLongValue],
+  'maxStorageBufferBindingSize':               [           , 134217728, kMaxUnsignedLongLongValue],
+  'minUniformBufferOffsetAlignment':           ['alignment',       256,                          ],
+  'minStorageBufferOffsetAlignment':           ['alignment',       256,                          ],
+
+  'maxVertexBuffers':                          [           ,         8,                          ],
+  'maxBufferSize':                             [           , 268435456, kMaxUnsignedLongLongValue],
+  'maxVertexAttributes':                       [           ,        16,                          ],
+  'maxVertexBufferArrayStride':                [           ,      2048,                          ],
+  'maxInterStageShaderComponents':             [           ,        60,                          ],
+  'maxInterStageShaderVariables':              [           ,        16,                          ],
+
+  'maxColorAttachments':                       [           ,         8,                          ],
+  'maxColorAttachmentBytesPerSample':          [           ,        32,                          ],
+
+  'maxComputeWorkgroupStorageSize':            [           ,     16384,                          ],
+  'maxComputeInvocationsPerWorkgroup':         [           ,       256,                          ],
+  'maxComputeWorkgroupSizeX':                  [           ,       256,                          ],
+  'maxComputeWorkgroupSizeY':                  [           ,       256,                          ],
+  'maxComputeWorkgroupSizeZ':                  [           ,        64,                          ],
+  'maxComputeWorkgroupsPerDimension':          [           ,     65535,                          ],
+});
+
+const kFeatures = new Set([
+  'bgra8unorm-storage',
+  'depth-clip-control',
+  'depth32float-stencil8',
+  'texture-compression-bc',
+  'texture-compression-etc2',
+  'texture-compression-astc',
+  'timestamp-query',
+  'indirect-first-instance',
+  'shader-f16',
+  'rg11b10ufloat-renderable',
+  'float32-filterable',
+]);
+window.k = kFeatures;
+
+const adapter = await navigator.gpu?.requestAdapter();
+
+function withShortSize(v) {
+  return v >= 1024 ? `${v} (${shortSize(v)})` : `${v}`;
+}
+
+const sortAlphabetically = (a, b) => a.toLowerCase() < b.toLowerCase() ? -1 : a.toLowerCase() > b.toLowerCase() ? 1 : 0;
+
+function getObjLikeKeys(objLike) {
+  const keys = [];
+  for (const key in objLike) {
+    keys.push(key);
+  }
+  return keys;
+}
+
+renderDiagrams({
+  limits(elem) {
+    const addRow = makeTable(elem, ['limit name', 'your device', 'min']);
+    for (const key of getObjLikeKeys(adapter.limits).sort(sortAlphabetically)) {
+      addRow([key, [adapter.limits[key] > kLimitInfo[key].default ? 'exceeds-limit' : '', withShortSize(adapter.limits[key])], withShortSize(kLimitInfo[key].default)]);
+    }
+  },
+
+  features(elem) {
+    const addRow = makeTable(elem, ['feature', 'your device']);
+    const allKeys = new Set([...kFeatures, ...adapter.features]);
+    for (const key of [...allKeys.keys()].sort(sortAlphabetically)) {
+      addRow([key, adapter.features.has(key) ? '✅' : kFeatures.has(key) ? '🚫' : '🤷‍♂️']);
+    }
+  },
+});
+
